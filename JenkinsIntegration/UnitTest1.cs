ef3c4bfa782781ea3b6789e01c75642fee6c9b84
Andrey Vlasov <lion32211@yandex.ru>
2023-07-23 18:15:50 +0200

    Try to run test in Jenkins + Selenoid

--- JenkinsIntegration/UnitTest1.cs
+++ JenkinsIntegration/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace JenkinsIntegration
@@ -14,20 +15,40 @@ namespace JenkinsIntegration
         [Test]
         public void Test1()
         {
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-            var options = new ChromeOptions();
+            // new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            // var options = new ChromeOptions();
 
-            options.AddUserProfilePreference("download.prompt_for_download", false);
-            options.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
-            options.AddUserProfilePreference("browser.download.manager.showWhenStarting", false);
-            options.AddUserProfilePreference("safebrowsing.enabled", "true");
-            options.AddArgument("no-sandbox");
-            options.AddArgument("disable-popup-blocking");
-            options.AddUserProfilePreference("profile.cookie_controls_mode", 0);
+            // options.AddUserProfilePreference("download.prompt_for_download", false);
+            // options.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
+            // options.AddUserProfilePreference("browser.download.manager.showWhenStarting", false);
+            // options.AddUserProfilePreference("safebrowsing.enabled", "true");
+            // options.AddArgument("no-sandbox");
+            // options.AddArgument("disable-popup-blocking");
+            // options.AddUserProfilePreference("profile.cookie_controls_mode", 0);
 
-            var specificDriver = new ChromeDriver(options);
+            // var specificDriver = new ChromeDriver(options);
 
-            specificDriver.Manage().Window.Maximize();
+            // specificDriver.Manage().Window.Maximize();
+
+
+            // SELENOID
+            var chromeOptions = new ChromeOptions();
+            //chromeOptions.AddUserProfilePreference("download.default_directory", directory);
+            chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
+            chromeOptions.AddUserProfilePreference("plugins.always_open_pdf_externally", true);
+            chromeOptions.AddUserProfilePreference("browser.download.manager.showWhenStarting", false);
+            chromeOptions.AddUserProfilePreference("safebrowsing.enabled", "true");
+            chromeOptions.AddArgument("no-sandbox");
+            chromeOptions.AddArgument("disable-popup-blocking");
+            chromeOptions.AddAdditionalOption("selenoid:options", new Dictionary<string, object>
+            {
+                ["enableLog"] = true,
+                ["enableVnc"] = true,
+                ["enableVideo"] = false
+            });
+            var specificDriver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chromeOptions.ToCapabilities());
+
+
             specificDriver.Navigate().GoToUrl("https://demoqa.com/");
             Thread.Sleep(5);
             specificDriver.Quit();
